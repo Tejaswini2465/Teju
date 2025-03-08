7e4b2c3284b296c1f199d8bc90645a75278ef989
Tejaswini2465 <vvce22ec0191@vvce.ac.in>
2025-03-08 10:16:23 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
-
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+  FILE *file = fopen("example.txt", "+r");
+
+  if (file == NULL)
+  {
+      printf("Error opening file for writing.\n");
+      return 1;
+  }
+  //fseek(file, 0, SEEK_SET);
+  fprintf(file, "Updated content: Hello, updated world!\n");
+  fclose(file);
+
+  printf("Data updated in file succesfully,\n");
+  return 0;
+
 }
+
+
